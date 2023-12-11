fb9948b6287b4bf65949c89c45b89646028b51ee
Nathalya Lucena Vieira de Melo <nlucena.dev@gmail.com>
2023-12-10 21:47:41 -0300

    Update Questão 4 e 5 da lista

--- src/Question4.java
+++ src/Question4.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class Question4 {
+
+    public static void main(String[] args) {
+
+        /* Escreva uma função que recebe o nome de um funcionário, seu
+           número de horas trabalhadas por mês, o valor da hora e calcula
+           o salário anual desse funcionário. (Mostre o valor com 2 casas
+           decimais após o ponto).*/
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Para calcular o salário anual, por favor, digite as informações solicitadas: ");
+
+        System.out.println("Digite o nome do funcionário: ");
+        String nome = input.nextLine();
+
+        System.out.println("Digite a quantidade de horas trabalhadas por mês: ");
+        double horasTrabalhadas = input.nextDouble();
+
+        System.out.println("Digite o valor da hora trabalhada: ");
+        double valorHora = input.nextDouble();
+
+        double salarioAnual = (calculadoraSalario(horasTrabalhadas,valorHora));
+        System.out.printf("O sálario anual de %s é de R$%.2f", nome, salarioAnual);
+
+        input.close();
+    }
+
+        public static double calculadoraSalario(double horasTrabalhadas, double valorHora) {
+
+        return horasTrabalhadas * valorHora * 12;
+    }
 }
--- src/Question5.java
+++ src/Question5.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class Question5 {
+
+        /* Joãozinho quer calcular e mostrar a quantidade de
+           litros de combustível gastos em uma viagem, ao utilizar um
+           automóvel que faz 12 KM/L. Para isso, ele gostaria que você o
+           auxiliasse através de um simples programa. Para efetuar o
+           cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e
+           a velocidade média durante a mesma (em km/h). Assim,
+           pode-se obter distância percorrida e, em seguida, calcular
+           quantos litros seriam necessários. Mostre o valor com 3 casas
+           decimais após o ponto. */
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            int desempenho = 12;
+
+            System.out.println("Quantas horas foram gastas na viagem?");
+            double horasViagem = input.nextDouble();
+
+            System.out.println("Qual a velocidade média durante a viagem?");
+            double velocidadeMedia = input.nextDouble();
+
+            double combustivelNecessario = (calculoCombustivel(horasViagem, velocidadeMedia, desempenho));
+
+            System.out.printf("A quantidade de combustível utilizada na viagem é de %.3f", combustivelNecessario);
+            input.close();
+        }
+
+        public static double calculoCombustivel(double horasViagem, double velocidadeMedia, int desempenho) {
+            return (horasViagem * velocidadeMedia) / desempenho;
+
+        }
+
 }
